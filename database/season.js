82ac8d8d18b5a4f6e11f46bdc325cc5b378f0ad4
Keith Boone <keithboone1@gmail.com>
2025-04-03 21:51:05 -0400

    Add 'current season' capability to website

--- database/season.js
+++ database/season.js
@@ -1,17 +1,38 @@
-import { db } from './database.js';
+import { db } from "./database.js";
 
 export async function loadCurrentSeason() {
-    return await db.get('SELECT * FROM season ORDER BY number DESC LIMIT 1');
+  return await db.get("SELECT * FROM season ORDER BY number DESC LIMIT 1");
 }
 
 export async function loadAllSeasons() {
-    return await db.all('SELECT number FROM season ORDER BY number DESC');
+  const seasons = await db.all(
+    "SELECT number FROM season ORDER BY number DESC"
+  );
+  const hasCurrentSeason = await db.get(
+    "SELECT matchup.room != 'finals' OR SUM(pairing.dead IS NULL and pairing.winner IS NULL) > 0 \
+     FROM matchup \
+     INNER JOIN pairing ON pairing.matchup = matchup.id \
+     GROUP BY matchup.id \
+     ORDER BY matchup.id DESC \
+     LIMIT 1"
+  );
+
+  return { seasons, hasCurrentSeason };
 }
 
 export async function saveNewSeason(season, length, playoffSize) {
-    await db.run('INSERT INTO season (number, current_week, regular_weeks, playoff_size) VALUES (?, 0, ?, ?)', season, length, playoffSize);
+  await db.run(
+    "INSERT INTO season (number, current_week, regular_weeks, playoff_size) VALUES (?, 0, ?, ?)",
+    season,
+    length,
+    playoffSize
+  );
 }
 
 export async function saveAdvanceWeek(season, week) {
-    await db.run('UPDATE season SET current_week = ? WHERE number = ?', week, season);
+  await db.run(
+    "UPDATE season SET current_week = ? WHERE number = ?",
+    week,
+    season
+  );
 }
--- website/frontend/app/welcome/welcome.tsx
+++ website/frontend/app/welcome/welcome.tsx
@@ -1,19 +1,43 @@
 import { Link } from "react-router";
 import type { Route } from "./+types/welcome";
 
-type SeasonsQuery = {
+type Season = {
   number: number;
-}[];
+};
+
+type SeasonsQuery = {
+  seasons: Season[];
+  hasCurrentSeason: boolean;
+};
 
 export default function Welcome({ loaderData }: Route.ComponentProps) {
+  const pastSeasons = loaderData.hasCurrentSeason
+    ? loaderData.seasons.slice(1)
+    : loaderData.seasons;
+
+  const currentSeason = loaderData.hasCurrentSeason
+    ? loaderData.seasons[0]
+    : null;
+
   return (
     <div className="flex flex-col items-center">
       <h1 className="mb-8 text-center">
         Mushi League, the ADV OU team tournament
       </h1>
+      {currentSeason && (
+        <>
+          <h2 className="mb-2 text-center">Current Season</h2>
+          <Link
+            to={`/season/${currentSeason.number}/schedule`}
+            className="border rounded-md text-center hover:bg-gray-50"
+          >
+            Season {currentSeason.number}
+          </Link>
+        </>
+      )}
       <h2 className="mb-2 text-center">Past Seasons</h2>
       <div className="flex flex-col gap-2 max-w-full w-sm">
-        {loaderData.map((s) =>
+        {pastSeasons.map((s) =>
           s.hasData ? (
             <Link
               key={s.number}
@@ -38,11 +62,14 @@ export async function loader() {
     await fetch("https://mushileague.gg/api/seasons")
   ).json()) as SeasonsQuery;
 
-  const lastNumber = rawData[0].number;
-  const firstNumber = rawData[rawData.length - 1].number;
+  const lastNumber = rawData.seasons[0].number;
+  const firstNumber = rawData.seasons[rawData.seasons.length - 1].number;
 
-  return new Array(lastNumber + 1).fill(null).map((_, i) => ({
-    number: lastNumber - i,
-    hasData: lastNumber - i >= firstNumber,
-  }));
+  return {
+    hasCurrentSeason: rawData.hasCurrentSeason,
+    seasons: new Array(lastNumber + 1).fill(null).map((_, i) => ({
+      number: lastNumber - i,
+      hasData: lastNumber - i >= firstNumber,
+    })),
+  };
 }
