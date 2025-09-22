fdd8672cf5f8527f436999175282b0b1c9cccb49
keithboone1 <keithboone1@gmail.com>
2025-09-22 08:04:25 -0400

    Fix float display... again

--- website/frontend/app/players/players.tsx
+++ website/frontend/app/players/players.tsx
@@ -80,7 +80,7 @@ export default function Players({ loaderData }: Route.ComponentProps) {
               <td className="px-2 py-0.5 font-semibold underline">
                 <NavLink to={`/players/${player.id}`}>{player.name}</NavLink>
               </td>
-              <td className="px-2 text-center">{player.stars.toFixed(2)}</td>
+              <td className="px-2 text-center">{Number(player.stars).toFixed(2)}</td>
               <td className="px-2 text-center">{player.total_seasons}</td>
               <td className="px-2 text-center">{player.season_wins}</td>
               <td className="px-2 text-center">{player.most_recent_season}</td>
