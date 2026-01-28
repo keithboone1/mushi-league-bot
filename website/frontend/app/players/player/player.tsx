9df45ff21e32dfce17f343e0994898a084a8bf8d
keithboone1 <keithboone1@gmail.com>
2026-01-27 21:01:51 -0500

    Fix bug with coaches breaking the player stats page

--- website/frontend/app/players/player/player.tsx
+++ website/frontend/app/players/player/player.tsx
@@ -164,10 +164,8 @@ export default function Season({
 
 export async function loader({ params: { playerId } }: Route.LoaderArgs) {
   const rawData = (await (
-    await fetch(`http://localhost:3001/api/players/${playerId}`)
+    await fetch(`https://mushileague.gg/api/players/${playerId}`)
   ).json()) as PlayerQuery;
-
-  console.log(rawData);
 
   const pairingsBySeason = rawData.pairings.reduce((accum, pairing) => {
     accum[pairing.season] = [...(accum[pairing.season] ?? []), pairing];
